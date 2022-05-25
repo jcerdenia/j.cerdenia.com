f6bdc1ac4cba33369f6529557fb430d1e0d5da1e
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-05-25 11:25:32 +0800

    Add redirect

--- dev-server.js
+++ dev-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { buildIndex, buildPage } = require("./build.js");
+const redirects = require("./redirects.json");
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,13 @@ app.get("/", (_req, res) => {
   res.send(buildIndex());
 });
 
-app.get("/:page", (req, res) => {
+app.get("/:page/", (req, res) => {
+  Object.keys(redirects).forEach((key) => {
+    if (key === req.params.page) {
+      res.redirect(redirects[key]);
+    }
+  });
+
   res.send(buildPage(req.params.page));
 });
 

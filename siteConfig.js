741df2254a814fef5af19496a0d2d596f485fcf2
Joshua Cerdenia <joshua.cerdenia@firstdigitalfinance.com>
2023-06-12 11:12:46 +0800

    feat(site-config): update links

--- siteConfig.js
+++ siteConfig.js
@@ -1,16 +1,16 @@
 const siteConfig = {
   links: [
     // eslint-disable-next-line prettier/prettier
-    ["GitHub", "https://github.com/joshuacerdenia", "bi bi-github"],
+    ["Source", "https://github.com/joshuacerdenia/j.cerdenia.com", "bi bi-github"],
     ["Music", "https://music.cerdenia.com", "bi bi-music-note"],
-    ["Gram", "https://instagram.com/joshuatopia", "bi bi-instagram"],
-    ["Contact", "mailto:joshua@cerdenia.com", "bi bi-envelope-fill"],
+    ["Twitter", "https://twitter.com/jc_erde", "bi bi-twitter"],
+    ["Mail", "mailto:joshua@cerdenia.com", "bi bi-envelope-fill"],
     ["RSS", "/feed", "bi bi-rss-fill"],
   ],
   metadata: {
     brand: "J. C.",
     copyright: `&#169; ${new Date().getFullYear()} Joshua Cerdenia`,
-    description: "A website",
+    description: "A Website",
     icon: "/images/icarus.jpeg",
     image: "/images/icarus.jpeg",
     siteUrl: "https://j.cerdenia.com",

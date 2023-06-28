9e0dffc245d52a69e2e0094978a13c1bb3db09be
Joshua Cerdenia <joshcerdenia@gmail.com>
2023-06-28 09:36:24 +0800

    refactor(list): make more concise

--- components/List.js
+++ components/List.js
@@ -1,13 +1,9 @@
 import HtmlBuilder from "../lib/HtmlBuilder.js";
 
-const List = (arr, map = (item) => item) => {
-  const items = [];
-
-  arr.forEach((_item) => {
-    const child = map(_item);
-    const item = HtmlBuilder("li").class("my-1").child(child);
-    items.push(item);
-  });
+const List = (_items, transform = (item) => item) => {
+  const items = _items.map((item) =>
+    HtmlBuilder("li").class("my-1").child(transform(item))
+  );
 
   return HtmlBuilder("ul").class("my-4").child(items.join(""));
 };

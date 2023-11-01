3245b06f5d3970d5efe43fadb65e7b0f272203c0
Joshua Cerdenia <joshcerdenia@gmail.com>
2023-11-01 19:49:51 +0800

    feat(accessor): improve needlessly

--- server-dev/state/accessors.js
+++ server-dev/state/accessors.js
@@ -4,10 +4,6 @@ const STATE_FILE_PATH = "./server-dev/state/state.json";
 
 const ENCODING = "utf8";
 
-export const setState = (state) => {
-  fs.writeFileSync(STATE_FILE_PATH, JSON.stringify(state), ENCODING);
-};
-
 export const getState = () => {
   try {
     const data = fs.readFileSync(STATE_FILE_PATH, ENCODING);
@@ -15,4 +11,9 @@ export const getState = () => {
   } catch {
     return {};
   }
+};
+
+export const setState = (data) => {
+  const state = { ...getState(), ...data };
+  fs.writeFileSync(STATE_FILE_PATH, JSON.stringify(state), ENCODING);
 };

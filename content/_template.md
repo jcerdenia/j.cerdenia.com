d4ffe25797f31d5c6843eda5ba09cb1462deb91d
Joshua Cerdenia <joshcerdenia@gmail.com>
2023-12-25 16:31:02 +0800

    feat: update automatic page creation

--- content/_template.md
+++ content/_template.md
@@ -4,6 +4,7 @@ date: "2023-01-01"
 pinned: false
 draft: true
 reverse_sections: false
+evaluate_js: false
 ---
 
 Write here.
--- scripts/new-page.js
+++ scripts/new-page.js
@@ -4,10 +4,33 @@ import matter from "gray-matter";
 import { CONTENT_PATH, ENCODING } from "../lib/files.js";
 import { formatDate } from "../lib/utils.js";
 
-const newPage = (slug) => {
-  const path = `${CONTENT_PATH}/${slug}.md`;
-  if (fs.existsSync(path)) {
+const createNewPage = (_slug, multi) => {
+  let slug = _slug;
+  const path = `${CONTENT_PATH}/${slug}`;
+
+  // Check for .md file
+  if (fs.existsSync(`${path}.md`)) {
     throw Error(`A file named "${slug}" already exists.`);
+  }
+
+  // Check for folder
+  if (fs.existsSync(path)) {
+    if (multi) {
+      const identifiers = fs
+        .readdirSync(path)
+        .filter((name) => name !== "index.md")
+        .map((name) => parseInt(name.replace(".md", ""), 10))
+        .sort((a, b) => a - b)
+        .reverse();
+
+      const identifier = (identifiers[0] || 0) + 1;
+      slug += `/${identifier}`;
+    } else {
+      throw Error(`A folder named "${slug}" already exists.`);
+    }
+  } else {
+    fs.mkdirSync(path);
+    slug += "/index";
   }
 
   const templatePath = `${CONTENT_PATH}/_template.md`;
@@ -16,15 +39,22 @@ const newPage = (slug) => {
   const date = formatDate(new Date(), { locale: "sv-SE" });
   markdown = markdown.replace(`date: "${data.date}"`, `date: "${date}"`);
 
-  fs.writeFileSync(path, markdown);
+  fs.writeFileSync(`${CONTENT_PATH}/${slug}.md`, markdown);
   console.log("\x1b[32m%s\x1b[0m", `Created new file: ${slug}.md`);
 };
 
-if (process.argv.length !== 3) {
-  console.log("Usage: node scripts/new-page.js <slug>");
+const { argv } = process;
+
+if (
+  (argv.length !== 3 && argv.length !== 4) ||
+  (argv.length === 4 && argv[3] !== "-")
+) {
+  console.log("Usage: npm run new <slug> (-)");
 } else {
   try {
-    newPage(process.argv[2]);
+    const slug = argv[2];
+    const multi = !!argv[3];
+    createNewPage(slug, multi);
   } catch (err) {
     console.error("\x1b[91m%s\x1b[0m", `Error: ${err.message}`);
   }

fa221521da5651bdfe68779f88631aaf1eada564
Joshua Cerdenia <joshcerdenia@gmail.com>
2025-01-04 22:50:10 +0800

    feat(list): increase space

--- components/List.js
+++ components/List.js
@@ -2,7 +2,7 @@ import HtmlBuilder from "../lib/HtmlBuilder.js";
 
 const List = (_items, transform = (item) => item) => {
   const items = _items.map((item) =>
-    HtmlBuilder("li").class("my-1").child(transform(item)),
+    HtmlBuilder("li").class("my-2").child(transform(item)),
   );
 
   return HtmlBuilder("ul").class("my-4").child(items.join(""));
